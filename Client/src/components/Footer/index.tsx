beb72585a29227563af3131e4e6a1f5b1074d921
Pottassiuw <pottassiu@proton.me>
2024-06-07 12:33:19 -0300

    Desktop Footer Done!

--- Client/src/components/Footer/index.tsx
+++ Client/src/components/Footer/index.tsx
@@ -3,28 +3,32 @@ import Logo from "../../assets/logo weeat.png";
 export default function () {
   return (
     <$.Footer>
-      <$.LogoContainer>
-        <$.LogoWrapper>
-          <$.Logo src={Logo} />
-          <$.LogoTitle>Weeat</$.LogoTitle>
-        </$.LogoWrapper>
-        <$.LogoSubtitle>
-          Seus restaurantes favoritos e seus pratos favoritos apenas aqui!
-        </$.LogoSubtitle>
-      </$.LogoContainer>
-      <$.ItensWrapper>
-        <$.ItensTitle>Item Title</$.ItensTitle>
-        <$.Links to="#">Link</$.Links>
-      </$.ItensWrapper>
-      <$.ItensWrapper>
-        <$.ItensTitle>Item Title</$.ItensTitle>
-        <$.Links to="#">Link</$.Links>
-      </$.ItensWrapper>
-      <$.ItensWrapper>
-        <$.ItensTitle>Serviços</$.ItensTitle>
-        <$.Links to="#">Cadastre sua Empresa!</$.Links>
-        <$.Links to="#">Cadastre seu Usuário!</$.Links>
-      </$.ItensWrapper>
+      <$.AllWrapper>
+        <$.LogoContainer>
+          <$.LogoWrapper>
+            <$.Logo src={Logo} />
+            <$.LogoTitle>Weeat</$.LogoTitle>
+          </$.LogoWrapper>
+          <$.LogoSubtitle>
+            Seus restaurantes favoritos e seus pratos favoritos apenas aqui!
+          </$.LogoSubtitle>
+        </$.LogoContainer>
+        <$.ItensWrapper>
+          <$.ItensTitle>Páginas</$.ItensTitle>
+          <$.Links to="#">Home</$.Links>
+          <$.Links to="#">Estabelecimentos</$.Links>
+        </$.ItensWrapper>
+        <$.ItensWrapper>
+          <$.ItensTitle>Usuário</$.ItensTitle>
+          <$.Links to="#">Cadastre o seu Usuário!</$.Links>
+          <$.Links to="#">Entre com seu Usuário!</$.Links>
+        </$.ItensWrapper>
+        <$.ItensWrapper>
+          <$.ItensTitle>Empresa</$.ItensTitle>
+          <$.Links to="#">Cadastre sua Empresa!</$.Links>
+          <$.Links to="#">Entre na sua Empersa!</$.Links>
+        </$.ItensWrapper>
+      </$.AllWrapper>
       <$.Copyright>
         <$.CopyrightText>
           © Weeat enterprises, All rights reserved 2024
--- Client/src/components/Footer/styles.ts
+++ Client/src/components/Footer/styles.ts
@@ -2,34 +2,34 @@ import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 export const Footer = styled.footer`
-    display: grid;
-    grid-template-columns: 300px 300px 300px 300px 300px;
-    grid-template-rows: 1fr 1fr;
+    display: flex;
     background-color: #2E2E2E;
     padding: 0px 4%;
-    border-top-left-radius: 1rem;
-    border-top-right-radius: 1rem;
     overflow: hidden;
-    align-items: center;
-    justify-content: center;
-    justify-items: center;
-    position: relative;
+    align-items: start;
+    height: 30dvh;
+    padding-top: 2rem;
+    flex-flow: row wrap;
 
     * {
         color: white;
     }
 `
+export const AllWrapper = styled.div`
+    display: flex;
+    width: 100%;
+`
 
 export const LogoContainer = styled.div`
-    grid-column: 1;
-    grid-row: 1;
+
     display: flex;
+    width: 40%;
     flex-flow: column nowrap;
 `
 export const LogoWrapper = styled.div`
-    grid-column: 1fr;
     display: flex;
     align-items: center;
+    
 `
 export const Logo = styled.img`
     width: 65px;
@@ -46,34 +46,37 @@ export const LogoSubtitle = styled.h2`
 export const ItensWrapper = styled.div`
     display: flex;
     flex-flow: column nowrap;
+    align-items: center;
+    width: 50%;
 `
 export const ItensTitle = styled.h1`
     font-size: ${p => p.theme.font.subtitle};
 `
-export const Links = styled(Link)``
+export const Links = styled(Link)`
+    padding: 5px 0px;
+`
 export const Copyright = styled.div`
     display: flex;
-    flex-flow: row nowrap;
     width: 100%;
+    align-items: center;
+    justify-content: center;
 `
 export const CopyrightText = styled.h1`
+    position: relative;
     font-weight: 500;
-    font-size: 15pt;
-    position: absolute;
-    bottom: 0;
-    margin-bottom: 2rem;
-    width: 100%;
-    left: 50%;
-    transform: translateX(-50%);
+    font-size: 14pt;
     text-align: center;
-    &::before{
+    width: 100%;
+
+    &::before {
         content: '';
-        width: 100%;
-        height: 2px;
-        background-color: white;
         position: absolute;
-        top: -20px;
+        width: 50%;
+        height: 2px;
+        background-color: #fff;
+        top: -10px;
         left: 50%;
         transform: translateX(-50%);
     }
+    
 `
