a6a8d0441f8fd59db624e05c7ec54885ad7206c8
Matheus reis <croriginalradioctive@gmail.com>
2024-06-09 20:29:21 -0300

    first commit

--- Client/src/pages/establishments/login/index.tsx
+++ Client/src/pages/establishments/login/index.tsx
@@ -1,4 +1,87 @@
+import * as $ from "./styles";
+import { useState, ChangeEvent } from "react";
+import axios from "axios";
+import NavBar from "../../../components/nav";
 
+export default function Login() {
+  const apiUrl = "https://localhost:4040/";
+  const [page, setPage] = useState(1);
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handlePages = () => {
+    setPage(2);
+  };
+
+  const handleChanges = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+  const sendLoginData = async () => {
+    try {
+      const response = await axios.post(`${apiUrl}login`, loginData);
+      console.log("Data send successfully!", response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <$.Screen>
+      <NavBar />
+      {page === 1 ? (
+        <$.Container>
+          <$.Wrapper>
+            <$.WrapperTitle>
+              <$.Title>Faça seu login!</$.Title>
+              <$.Subtitle>Entre com suas credenciais</$.Subtitle>
+            </$.WrapperTitle>
+            <$.Form
+              onSubmit={(e) => {
+                e.preventDefault();
+                sendLoginData();
+              }}
+            >
+              <$.WrapperInput>
+                <$.Label>E-mail</$.Label>
+                <$.Input
+                  type="text"
+                  name="email"
+                  value={loginData.email}
+                  onChange={handleChanges}
+                  required
+                  placeholder="ex: email@email.com"
+                />
+              </$.WrapperInput>
+              <$.WrapperInput>
+                <$.Label>Senha</$.Label>
+                <$.Input
+                  type="password"
+                  name="password"
+                  value={loginData.password}
+                  onChange={handleChanges}
+                  required
+                  placeholder="Mínimo 6 caracteres"
+                />
+              </$.WrapperInput>
+              <$.SubmitButton type="submit" onClick={handlePages}>
+                Entrar
+              </$.SubmitButton>
+            </$.Form>
+          </$.Wrapper>
+        </$.Container>
+      ) : page === 2 ? (
+        <$.Rcontainer></$.Rcontainer>
+      ) : null}
+    </$.Screen>
+  );
+}
 export default function index() {
   return (
     <>
--- Client/src/router.tsx
+++ Client/src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/users/login";
+import StoreLogin from "./pages/establishments/login";
 import UserRegister from "./pages/users/register";
 import StoreRegister from "./pages/establishments/register";
 import Home from "./pages/home";
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
       { path: "users/login", element: <Login /> },
       { path: "users/register", element: <UserRegister /> },
       { path: "stores/register", element: <StoreRegister /> },
+      { path: "stores/login", element:  <StoreLogin />}
     ],
   },
 ]);
